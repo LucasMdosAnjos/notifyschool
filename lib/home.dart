cfaf85b56bf79b6c384f42e5f3bb9e9c381e52e3
Lucas Anjos <lucasoneplanet@gmail.com>
2020-10-21 20:36:32 -0300

    ajustes

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:notifyschool/login.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +8,18 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var _auth = FirebaseAuth.instance;
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(microseconds: 1)).then((value) {
+      if (_auth.currentUser == null) {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (_) => Login()));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/login.dart
+++ lib/login.dart
@@ -6,8 +6,29 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    String email = '';
+    String senha = '';
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          caixaDeTexto(),
+          caixaDeTexto()
+        ],
+      ),
+    );
+  }
+
+  Widget caixaDeTexto(){
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(),
+    );
   }
 }
